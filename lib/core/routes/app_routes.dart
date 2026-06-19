394598fa5c180420f605cfcc19e40d0faa8a2c82
Mahmoud <Sshss@example.comecho>
2026-06-19 12:58:35 +0300

    0

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -3,8 +3,6 @@ import 'dart:io';
 import 'package:track_flowers_app/config/dependency_injection/di.dart';
 import 'package:track_flowers_app/core/data/data_sources/auth_local_data_source.dart';
 import 'package:track_flowers_app/core/routes/routes.dart';
-import 'package:track_flowers_app/features/auth_module/presentation/view/pages/auth_module_page.dart';
-import 'package:track_flowers_app/features/auth_module/presentation/view/pages/onboarding_driver_page.dart';
 import 'package:track_flowers_app/features/login/presentation/view/pages/login_page.dart';
 import 'package:track_flowers_app/features/spalsh/splash_page.dart';
 import 'package:track_flowers_app/features/main/presentation/screens/main_view.dart';
