e95a618e4f12d3cbbd20f7e1a8572f4a0c309c6c
Ahmed Yosry <a7med.elesawy0155066@gmail.com>
2026-06-20 14:39:56 +0300

    chore: update local pub cache paths in configuration files

--- lib/config/dependency_injection/di.config.dart
+++ lib/config/dependency_injection/di.config.dart
@@ -18,6 +18,22 @@ import 'package:internet_connection_checker_plus/internet_connection_checker_plu
 
 import '../../core/api/datasources/auth_local_data_source_impl.dart' as _i424;
 import '../../core/data/data_sources/auth_local_data_source.dart' as _i759;
+import '../../features/auth_module/api/api_client/auth_module_api_client.dart'
+    as _i419;
+import '../../features/auth_module/api/datasources/auth_module_local_data_source_impl.dart'
+    as _i1034;
+import '../../features/auth_module/api/datasources/auth_module_remote_data_source_impl.dart'
+    as _i428;
+import '../../features/auth_module/data/datasources/auth_module_local_data_source_contract.dart'
+    as _i435;
+import '../../features/auth_module/data/datasources/auth_module_remote_data_source_contract.dart'
+    as _i72;
+import '../../features/auth_module/data/repositories/auth_module_repository_impl.dart'
+    as _i848;
+import '../../features/auth_module/domain/repositories/auth_module_repository.dart'
+    as _i1015;
+import '../../features/auth_module/presentation/view_model/cubit/auth_module_cubit.dart'
+    as _i575;
 import '../../features/login/api/api_client/login_api_client.dart' as _i395;
 import '../../features/login/api/datasources/login_local_data_source_impl.dart'
     as _i438;
@@ -63,6 +79,7 @@ extension GetItInjectableX on _i174.GetIt {
   }) {
     final gh = _i526.GetItHelper(this, environment, environmentFilter);
     final dioModule = _$DioModule();
+    gh.factory<_i575.AuthModuleCubit>(() => _i575.AuthModuleCubit());
     gh.singleton<_i361.Dio>(() => dioModule.dio());
     gh.lazySingleton<_i558.FlutterSecureStorage>(
       () => dioModule.secureStorage(),
@@ -71,7 +88,19 @@ extension GetItInjectableX on _i174.GetIt {
     gh.lazySingleton<_i161.InternetConnection>(
       () => dioModule.internetConnection(),
     );
+    gh.lazySingleton<_i419.AuthModuleApiClient>(
+      () => _i419.AuthModuleApiClient(),
+    );
     gh.lazySingleton<_i679.HomeCubit>(() => _i679.HomeCubit());
+    gh.factory<_i72.AuthModuleRemoteDataSourceContract>(
+      () => _i428.AuthModuleRemoteDataSourceImpl(),
+    );
+    gh.factory<_i435.AuthModuleLocalDataSourceContract>(
+      () => _i1034.AuthModuleLocalDataSourceImpl(),
+    );
+    gh.factory<_i1015.AuthModuleRepository>(
+      () => _i848.AuthModuleRepositoryImpl(),
+    );
     gh.factory<_i395.LoginApiClient>(
       () => _i395.LoginApiClient(gh<_i361.Dio>()),
     );
