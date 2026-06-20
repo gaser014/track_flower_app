34ad545a6bb9a02129c4894284a9e0e6f3ef27ca
gaser014 <gaser.mohamed158@gmail.com>
2026-06-20 14:24:16 +0300

    feat(auth_module): implement base structure for auth module
    - Add AuthModuleApiClient with retrofit and injectable integration
    - Create AuthModuleCubit extending BaseCubit with initial state and event handler stub
    - Define sealed AuthModuleEvent class for handling auth events
    - Implement AuthModuleState with equatable support and copyWith method
    - Annotate local and remote data source implementations with injectable
    - Annotate AuthModuleRepositoryImpl with injectable as repository interface

--- lib/features/auth_module/api/api_client/auth_module_api_client.dart
+++ lib/features/auth_module/api/api_client/auth_module_api_client.dart
@@ -1,1 +1,7 @@
-  class AuthModuleApiClient {}
+import 'package:injectable/injectable.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:track_flowers_app/config/api/end_points.dart';
+
+@lazySingleton
+@RestApi(baseUrl: EndPoints.baseUrl)
+class AuthModuleApiClient {}
--- lib/features/auth_module/data/repositories/auth_module_repository_impl.dart
+++ lib/features/auth_module/data/repositories/auth_module_repository_impl.dart
@@ -1,3 +1,5 @@
+import 'package:injectable/injectable.dart';
 import 'package:track_flowers_app/features/auth_module/domain/repositories/auth_module_repository.dart';
 
+@Injectable(as: AuthModuleRepository)
 class AuthModuleRepositoryImpl implements AuthModuleRepository {}
