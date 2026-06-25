a4d042009a44e21a90520ad22384c14d0b35aff9
Ahmed Yosry <a7med.elesawy0155066@gmail.com>
2026-06-25 11:04:44 +0300

    refactor: update application ID and package namespace to com.elevate.trackFlowers

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.track_flowers_app"
+    namespace = "com.elevate.trackFlowers"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.track_flowers_app"
+        applicationId = "com.elevate.trackFlowers"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
